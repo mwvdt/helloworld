31d29ee73145d78982f76af8033038203bff1940
Marten <m.vandertempel@vbpk.eu>
2013-01-18 19:47:13 +0100

    master
    Signed-off-by: Marten <m.vandertempel@vbpk.eu>

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -20,7 +20,7 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World Marten development");
+        System.out.println("Hello, World Marten master");
     }
 
 }
